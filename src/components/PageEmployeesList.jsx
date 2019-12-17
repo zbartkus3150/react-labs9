fd640527337a475738ee4783d995a0c8446ad3cd
zbartkus <zbartkuss3150@gmail.com>
2019-12-17 19:23:33 +0100

    Lab10Ex3 & Lab10Ex4

--- src/components/PageEmployeesList.jsx
+++ src/components/PageEmployeesList.jsx
@@ -17,6 +17,9 @@ class PageEmployeesList extends React.Component {
   }
 
   componentDidMount() {
+    if (this.props.isFetched === true) {
+      return;
+    }
     this.setState({ isLoading: true });
     fetch('http://localhost:3004/employees')
     .then((data) => data.json())
@@ -51,7 +54,8 @@ class PageEmployeesList extends React.Component {
 
 const mapStateToProps = (state /*, ownProps*/) => {
   return {
-    employees: state.employees
+    employees: state.employees,
+    isFetched: state.isFetched
   }
 }
 
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,6 +3,7 @@ import { EMPLOYEE_CREATED } from './constants';
 
 export const initialState = {
   employees: [],
+  isFetched: false
 };
 
 // Read this: https://redux.js.org/basics/reducers
@@ -12,11 +13,12 @@ const appReducer = (state = initialState, action) => {
     case EMPLOYEES_LOADED: {
       const { employees } = action.payload;
       // CAREFUL: You can't modify state variable directly.
-      return Object.assign({}, state, { employees });
+      return Object.assign({}, state, { employees, isFetched: true });
     }
     case EMPLOYEE_CREATED: {
-      const { employee } = action.payload;
-      return { ...state, employee };
+      const newEmployee = action.payload.employee;
+      const newEmployees = [...state.employees, newEmployee];
+      return { ...state, employees: newEmployees };
     }
     default:
         return state
